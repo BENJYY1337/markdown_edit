d06f709b665bd323ef45fad73eac1202480f8b02
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-11-20 11:24:04 +0100

    write html with jsx

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea
+            className='form-control'
+            rows="35" />
+        </div>
+        <div className="col-sm-6">
+          <h1>Résultat</h1>
+        </div>
+      </div>
     </div>
   );
 }
