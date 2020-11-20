3b3427c87b4912f65fbb0ddb2b759674b8fec31e
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-11-20 13:11:12 +0100

    use and modify hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react'
 import { sampleText } from './sampleText'
 
 function App() {
+
+  const [text, setText] = useState(sampleText)
+
+  const handleChange = (event) => {
+    const text = event.target.value
+    setText(text)
+  }
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-6">
-          <textarea
-            value={sampleText}
-            className='form-control'
-            rows="35" />
+          <textarea onChange={handleChange} value={text} className='form-control' rows="35" />
         </div>
         <div className="col-sm-6">
-          <div>
-            {sampleText}
-          </div>
+          <div>{sampleText}</div>
         </div>
       </div>
     </div>
